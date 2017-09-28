af94816726e6af9b19cd341cbc92ea7ed6900600
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-09-27 21:55:06 -0700

    add connection info

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 //Dependancies
 var mysql = require("mysql");
+var connection;
 
-//Initialize Connection
-var connection = mysql.createConnection({
-	host: "localhost",
-	port: 8889,
-	user: "root",
-	password: "root",
-	database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+	 connection = mysql.createConnection({
+		host: "localhost",
+		port: 8889,
+		user: "root",
+		password: "root",
+		database: "burgers_db"
+	});
+}
 
 //Start Connection
 connection.connect(function(err){
